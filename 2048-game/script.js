d6e4abef549104b5acf8150da09d8f0c91ac16b5
Boris Yarmulnik <boris.yarmoulnik@gmail.com>
2024-05-29 19:13:59 +0300

    feat: change value of color & background at tile

--- 2048-game/script.js
+++ 2048-game/script.js
@@ -6,3 +6,4 @@ const gameBoard = document.getElementById('game-board');
 const grid = new Grid(gameBoard);
 
 grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
+grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
